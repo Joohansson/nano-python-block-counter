1fbafeeb7eca42bddcbe42327e70f39178579f18
joohansson <d84@outlook.com>
2019-07-24 09:05:44 +0200

    Cemented and CPS from RPC

--- checkBlocks.py
+++ checkBlocks.py
@@ -5,7 +5,6 @@
 from nano import RPCClient #pip3 install nano-python
 import schedule #pip3 install schedule
 import asyncio #pip3 install asyncio
-import websockets #pip3 install websockets
 
 #Standard python
 import json
@@ -16,15 +15,11 @@ import os
 
 #Settings
 rpc = RPCClient('http://[::1]:55000') #nano node address and port(I use this in node config: "::ffff:127.0.0.1"). You can also try with "http://[::1]:55000"
-websocketURL = '[::1]:57000' #websocket url defined in nano config.json (must be enabled in config if you want confirmation per second)
-tpsInterval = [10, 60, 300, 600, 3600] #intervals defined in seconds for local logging (any set of numbers work)
+tpsInterval = [30, 300, 600, 3600] #intervals defined in seconds for local logging (any set of numbers work)
 enableStatfiles = False #set to True (not true) to enable saving to log files
 enableOutput = True #set to True (not true) to enable console logs
-enableCPS = True #set to True to enable confirmations per second from websocket subscription
-skipInvalid = False #filter out inactive confirmations from websocket
+includeCemented = True #if include cemented block count from node RPC command (v19+)
 statsPath = 'stats' #path to statfile basename (extension will be added automatically and two files for each interval will be created)
-includeCemented = False #if include cemented block count from node cli command (v19+) (may need sudo access to the node)
-nodePath = '/root/nano-node-19.0.4-Linux/nano_node' #absolute path to the nano node if using "includeCemented"(does not work with docker)
 
 #If sending to server to collect stats
 enableServer = False #set to True (not true)
@@ -38,18 +33,18 @@ serverURI = 'https://beta.nanoticker.info/tps_beta_push.php' #php code to accept
 #Vars (dont touch)
 headers = { "charset" : "utf-8", "Content-Type": "application/json" }
 countOld = []
-confCount = [] #contains confirmation count for different intervals
-serverConf = 0 #number of confirmations reported by websocket
-serverConfRolling = [] #rolling window of confirmations
+countOldCPS = []
+serverCPSRolling = []
+countOldCPSServer = 0
 
 for i in tpsInterval:
   countOld.append(0)
-  confCount.append(0) #for CPS subscription
+  countOldCPS.append(0)
 
 #Sheduled job for TPS
 def jobRPC(interval):
   global countOld
-  global confCount
+  global countOldCPS
 
   #Get latest block count from nano RPC service
   if not rpc:
@@ -58,38 +53,49 @@ def jobRPC(interval):
   arred = lambda x,n : x*(10**n)//1/(10**n) #decimal point function | arred(3.1415,2) => 2 decimals
 
   try:
-    blockCount = rpc.block_count()
-    blkCount = blockCount['count']
-    blkUnch = blockCount['unchecked']
+    #get cemented block count
+    if (includeCemented):
+      params = {
+          "action": "block_count",
+          "include_cemented": "true"
+      }
+      resp = requests.post(url=nodeUrl, json=params)
+      blockCount = resp.json()
+      blkCount = int(blockCount['count'])
+      blkUnch = int(blockCount['unchecked'])
+      cemented = int(blockCount['cemented'])
+
+      #Calculate CPS based on previous values
+      if countOldCPS[interval] == 0: #first iteration => cps=0
+        for i,into in enumerate(countOldCPS): #initialize memory
+          countOldCPS[i] = cemented
+        cps = 0
+      else:
+        cps = (cemented - countOldCPS[interval]) / tpsInterval[interval] #tps based on previous iteration
+      countOldCPS[interval] = cemented #update value for next iteration
+
+    else:
+      blockCount = rpc.block_count()
+      blkCount = blockCount['count']
+      blkUnch = blockCount['unchecked']
+      cemented = 0
+      cps = 0
+
     peers = rpc.peers()
     timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 
     #Calculate TPS based on previous values
     if countOld[interval] == 0: #first iteration => tps=0
-      for i,int in enumerate(countOld): #initialize memory
+      for i,into in enumerate(countOld): #initialize memory
         countOld[i] = blkCount
       tps = 0
     else:
       tps = (blkCount - countOld[interval]) / tpsInterval[interval] #tps based on previous iteration
     countOld[interval] = blkCount #update value for next iteration
 
-    cps = confCount[interval] / tpsInterval[interval]
-
-    #get cemented block count
-    if (includeCemented):
-      response = os.popen(nodePath + ' --debug_cemented_block_count').read()
-      cemented = response.split(": ",1)[1].replace("\n","")
-
-      #Create json and csv definitions
-      entryJSON = {'time':timestamp, 'count':blkCount, 'unchecked':blkUnch, 'cemented':cemented, 'peers': len(peers), 'interval': tpsInterval[interval], 'tps':arred(tps,2), 'cps':arred(cps,2)}
-      entryCSV = [timestamp, str(blkCount), str(blkUnch), str(cemented), str(len(peers)), str(tpsInterval[interval]), str(arred(tps,2)), str(arred(cps,2))]
-
-    else:
-      #Create json and csv definitions
-      entryJSON = {'time':timestamp, 'count':blkCount, 'unchecked':blkUnch, 'peers': len(peers), 'interval': tpsInterval[interval], 'tps':arred(tps,2), 'cps':arred(cps,2)}
-      entryCSV = [timestamp, str(blkCount), str(blkUnch), str(len(peers)), str(tpsInterval[interval]), str(arred(tps,2)), str(arred(cps,2))]
-
-    confCount[interval] = 0 #reset for next interval measure
+    #Create json and csv definitions
+    entryJSON = {'time':timestamp, 'count':blkCount, 'unchecked':blkUnch, 'cemented':cemented, 'peers': len(peers), 'interval': tpsInterval[interval], 'tps':arred(tps,2), 'cps':arred(cps,2)}
+    entryCSV = [timestamp, str(blkCount), str(blkUnch), str(cemented), str(len(peers)), str(tpsInterval[interval]), str(arred(tps,2)), str(arred(cps,2))]
 
     if enableOutput:
       print(json.dumps(entryJSON))
@@ -123,29 +129,51 @@ def jobRPC(interval):
 
 #Job for pushing to server
 def jobRPCServer():
-  global serverConf
-  global serverCPSInterval
-  global serverConfRolling
+  global serverCPSRolling
+  global countOldCPSServer
 
   #Get latest block count from nano RPC service
   if not rpc:
     return
 
   try:
-    blockCount = rpc.block_count()
-    blkCount = blockCount['count']
-    blkUnch = blockCount['unchecked']
+    #get cemented block count
+    if (includeCemented):
+      params = {
+          "action": "block_count",
+          "include_cemented": "true"
+      }
+      resp = requests.post(url=nodeUrl, json=params)
+      blockCount = resp.json()
+      blkCount = int(blockCount['count'])
+      blkUnch = int(blockCount['unchecked'])
+      cemented = int(blockCount['cemented'])
+
+      #Calculate CPS based on previous values
+      if countOldCPSServer == 0: #first iteration => cps=0
+        serverCPS = 0
+      else:
+        serverCPS = (cemented - countOldCPSServer) / serverInterval #tps based on previous iteration
+      countOldCPSServer = cemented #update value for next iteration
+
+      serverCPSRolling.append(cemented)
+      if (len(serverCPSRolling) > (serverCPSInterval / serverInterval + 1)):
+        serverCPSRolling.pop(0)
+      if (len(serverCPSRolling) - 1 > 0):
+        serverCPS = (serverCPSRolling[-1] - serverCPSRolling[0]) / ((len(serverCPSRolling) - 1) * serverInterval)
+      else:
+        serverCPS = 0
+
+    else:
+      blockCount = rpc.block_count()
+      blkCount = blockCount['count']
+      blkUnch = blockCount['unchecked']
+      cemented = 0
+      serverCPS = 0
+
     peers = rpc.peers()
     version = rpc.version()['node_vendor']
     unixtime = time.time()
-
-    serverConfRolling.append(serverConf)
-    if (len(serverConfRolling) > (serverCPSInterval / serverInterval + 1)):
-      serverConfRolling.pop(0)
-    if (len(serverConfRolling) - 1 > 0):
-      serverCPS = (serverConfRolling[-1] - serverConfRolling[0]) / ((len(serverConfRolling) - 1) * serverInterval)
-    else:
-      serverCPS = 0
 
     postJSON = {'time':unixtime, 'count':blkCount, 'unchecked':blkUnch, 'interval': serverInterval, 'id': secret, 'name': name, 'peers': len(peers), 'version': version, 'cps': serverCPS}
 
@@ -167,58 +195,13 @@ def jobRPCServer():
     return
 
 #Define scheduled job
-for i,int in enumerate(tpsInterval):
-  schedule.every(int).seconds.do(jobRPC, interval=i)
+for i,into in enumerate(tpsInterval):
+  schedule.every(into).seconds.do(jobRPC, interval=i)
 
 #Schedule job for sending stat to server
 if enableServer:
   schedule.every(serverInterval).seconds.do(jobRPCServer)
   jobRPCServer() #Init values one time directly
-
-#Subscription service via websockets
-def subscription(topic: str, ack: bool=False, options: dict=None):
-    d = {"action": "subscribe", "topic": topic, "ack": ack}
-    if options is not None:
-        d["options"] = options
-    return d
-
-async def cpsTask():
-    try:
-      async with websockets.connect(f"ws://{websocketURL}") as websocket:
-        # Subscribe to confirmations
-        # You can also add options here following instructions in
-        #   https://github.com/nanocurrency/nano-node/wiki/WebSockets
-
-        if (skipInvalid):
-          await websocket.send(json.dumps(subscription("confirmation", ack=True, options={"confirmation_type": "active"})))
-        else:
-          await websocket.send(json.dumps(subscription("confirmation", ack=True)))
-
-        print(await websocket.recv())  # ack
-
-        init = True
-        global serverConf
-        global confCount
-
-        while 1:
-          rec = json.loads(await websocket.recv())
-          topic = rec.get("topic", None)
-          if topic:
-            message = rec["message"]
-
-            if topic == "confirmation":
-                #print("Block confirmed: {}".format(message))
-                #Start the clock on first confirmation
-                if init:
-                  init = False
-
-                for i,conf in enumerate(confCount):
-                  confCount[i] = confCount[i] + 1
-
-                serverConf = serverConf + 1
-
-    except ConnectionRefusedError:
-      print("Error connecting to websocket server. Make sure you have enabled it in ~/Nano/config.json")
 
 async def tpsTask():
   jobRPC(interval=0) #init
@@ -227,7 +210,5 @@ async def tpsTask():
     await asyncio.sleep(0.01)
 
 loop = asyncio.get_event_loop()
-if enableCPS:
-  loop.create_task(cpsTask())
 loop.create_task(tpsTask())
 loop.run_forever()
